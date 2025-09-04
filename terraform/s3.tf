63d69db2c454b98022a257e69227bca53a1946cc
Seiya-Nakagawa <aibdlnew1.work@gmail.com>
2025-09-04 22:35:30 +0900

    初回コミット

---
+++ terraform/s3.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "s3_link_checker_results" {
+  bucket = "${var.system_name}-${var.env}-s3-link-checker-results"
+
+  tags = {
+    Name        = "${var.system_name}-${var.env}-s3-link-checker-results",
+    SystemName  = var.system_name,
+    Env         = var.env,
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_link_checker_results" {
+  bucket = aws_s3_bucket.link_checker_results.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
---
+++ terraform/versions.tf
@@ -0,0 +1,34 @@
+# versions.tf
+
+# Terraformの実行環境に関する設定
+terraform {
+  required_version = ">= 1.12.2"
+
+  # Terraform Cloudをバックエンドとして設定
+  cloud {
+    organization = "aibdlnew1-organization"
+
+    # このコードがどのワークスペース群に属するかを示すタグを設定
+    workspaces {
+      name = "aws-blogcheker-prd"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.30"
+    }
+  }
+}
+
+# プライマリリージョン (東京)
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+# グローバルサービス用プロバイダ (バージニア北部)
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
