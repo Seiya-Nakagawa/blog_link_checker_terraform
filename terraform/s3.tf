7b2e293e0dd9a7afa86ba0116305a950fbed64d2
Seiya-Nakagawa <aibdlnew1.work@gmail.com>
2025-09-06 13:20:13 +0900

    zzzz

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -16,17 +16,17 @@ resource "aws_s3_bucket_versioning" "versioning_link_checker_results" {
 }
 
 # S3 Bucket Notification to trigger Lambda
-resource "aws_s3_bucket_notification" "s3_lambda_trigger" {
-  bucket = aws_s3_bucket.s3_link_checker.id
+# resource "aws_s3_bucket_notification" "s3_lambda_trigger" {
+#   bucket = aws_s3_bucket.s3_link_checker.id
 
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.link_checker_lambda.arn
-    events              = ["s3:ObjectCreated:Put"]
-    filter_prefix       = "gas_urls/"
-    filter_suffix       = "urls_list.json"
-  }
+#   lambda_function {
+#     lambda_function_arn = aws_lambda_function.link_checker_lambda.arn
+#     events              = ["s3:ObjectCreated:Put"]
+#     filter_prefix       = "gas_urls/"
+#     filter_suffix       = "urls_list.json"
+#   }
 
-  depends_on = [
-    aws_lambda_permission.allow_s3_to_call_lambda
-  ]
-}
+#   depends_on = [
+#     aws_lambda_permission.allow_s3_to_call_lambda
+#   ]
+# }
