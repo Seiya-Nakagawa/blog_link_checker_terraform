49f1527a248b29ecdb2dda9c60d5ffd65c9af9f8
Seiya-Nakagawa <aibdlnew1.work@gmail.com>
2025-09-27 17:53:22 +0900

    cccc

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -26,3 +26,28 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_s3" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
+
+
+# IAM Role for CloudWatchLogs
+resource "aws_iam_role" "cwlogs_exec_role" {
+  name = "blog-link-checker-cwlogs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "cloudwatch.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# IAM Policy for cwlogs
+resource "aws_iam_role_policy_attachment" "cwlogs_policy_cwlogs" {
+  role       = aws_iam_role.cwlogs_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
+}
