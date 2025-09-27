6e636f5dc0012f5bd5c8df41d0a215a9e57e3959
Seiya-Nakagawa <aibdlnew1.work@gmail.com>
2025-09-27 18:21:22 +0900

    aaaa

--- terraform/cloudwatch_logs.tf
+++ terraform/cloudwatch_logs.tf
@@ -10,15 +10,3 @@ resource "aws_cloudwatch_log_group" "lambda_log_group" {
     Env         = var.env,
   }
 }
-
-# ----------------------------------------------------
-# CloudWatch Logs Subscription Filter for SNS
-# ----------------------------------------------------
-resource "aws_cloudwatch_log_subscription_filter" "log_subscription_filter" {
-  name            = "${var.system_name}-${var.env}-log-subscription-filter"
-  log_group_name  = aws_cloudwatch_log_group.lambda_log_group.name
-  filter_pattern  = "{ $.level = \"WARNING\" || $.level = \"ERROR\" }"
-  destination_arn = aws_sns_topic.sns_topic_system.arn
-  role_arn        = aws_iam_role.cwlogs_exec_role.arn
-  depends_on      = [aws_sns_topic.sns_topic_system]
-}
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -27,27 +27,3 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_s3" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
-
-# IAM Role for CloudWatchLogs
-resource "aws_iam_role" "cwlogs_exec_role" {
-  name = "blog-link-checker-cwlogs-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "logs.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# IAM Policy for cwlogs
-resource "aws_iam_role_policy_attachment" "cwlogs_policy_cwlogs" {
-  role       = aws_iam_role.cwlogs_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
-}
