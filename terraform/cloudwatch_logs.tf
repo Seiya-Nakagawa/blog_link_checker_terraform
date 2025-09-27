c2203ddb09f36520378f5512afa1db8fc50c8af5
Seiya-Nakagawa <aibdlnew1.work@gmail.com>
2025-09-27 17:54:54 +0900

    dddd

--- terraform/cloudwatch_logs.tf
+++ terraform/cloudwatch_logs.tf
@@ -15,10 +15,10 @@ resource "aws_cloudwatch_log_group" "lambda_log_group" {
 # CloudWatch Logs Subscription Filter for SNS
 # ----------------------------------------------------
 resource "aws_cloudwatch_log_subscription_filter" "log_subscription_filter" {
-  name           = "${var.system_name}-${var.env}-log-subscription-filter"
-  log_group_name = aws_cloudwatch_log_group.lambda_log_group.name
-  filter_pattern = "{ $.level = \"WARNING\" || $.level = \"ERROR\" }"
+  name            = "${var.system_name}-${var.env}-log-subscription-filter"
+  log_group_name  = aws_cloudwatch_log_group.lambda_log_group.name
+  filter_pattern  = "{ $.level = \"WARNING\" || $.level = \"ERROR\" }"
   destination_arn = aws_sns_topic.sns_topic_system.arn
-
-  depends_on = [aws_sns_topic.sns_topic_system]
+  role_arn        = aws_iam_role.cwlogs_exec_role.arn
+  depends_on      = [aws_sns_topic.sns_topic_system]
 }
